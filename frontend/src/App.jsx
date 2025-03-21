636a6d437cc43021b8e139d4d7eb3158a25c0868
Charvi Basappa <charvibasappa98@gmail.com>
2025-03-20 22:09:51 -0700

    Worked on Navbar with already given logo from assets.logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,16 @@ import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
+import Cart from "./pages/cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />}></Route>
